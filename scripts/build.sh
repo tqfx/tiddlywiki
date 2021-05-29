9b7f9b910baaba9a9165dba8eb517cc941c934a3
tqfx <tqfx@foxmail.com>
2021-05-30 00:21:35 +0800

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,5 @@ SOURCE=.
 FILES=files
 
 tiddlywiki $SOURCE --output $OUTPUT --build
-#cp -r $FILES $OUTPUT
-#cp -r $FILES $OUTPUT/static
+cp -r $FILES $OUTPUT || exit 0
+cp -r $FILES $OUTPUT/static || exit 0
